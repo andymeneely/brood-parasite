8c059831b699ebf90a9043b9d68e876731cfa642
Andy Meneely <andy@se.rit.edu>
2025-02-28 20:34:21 -0500

    action cards in good shape

--- src/actions.rb
+++ src/actions.rb
@@ -1,33 +1,48 @@
-require 'squib'
+# require 'squib'
+require_relative '../../squib/lib/squib'
 require_relative 'version'
 
 data = Squib.csv file: 'data/actions.csv'
 
 Squib::Deck.new(cards: data.nrows) do
-  background color: :white
   use_layout file: 'layouts/actions.yml'
 
+  background color: :white
+  svg file: 'action_backdrop.svg'
+
   text str: data.name, layout: :name
-  text str: data.desc, layout: :desc
+  text str: data.type, layout: :type
 
-  # type = {};
-  # data['type'].each_with_index{ |t, i| (type[t] ||= []) << i}
-  # text range: type['Thinker'],
-  #      str: 'Only for Thinkers!',
-  #      x:25, y: 500
-  #
+  text(layout: :time_str,
+       str: data.time.map { |t| t > 0 ? "#{t} :time:" : ''}) do |embed|
+    embed.svg key: ':time:', layout: :time_str_embed
+  end
+
+  text str: data.desc, layout: :desc do |embed|
+    embed.svg key: ':scan:', layout: :scan_embed_icon
+    embed.svg key: ':broadcast:', layout: :broadcast_embed_icon
+    embed.svg key: ':crack:', layout: :crack_embed_icon
+    embed.svg key: ':script:', layout: :script_embed_icon
+    embed.svg key: ':time:', layout: :time_embed_icon
+  end
+
+  text str: data.cpu, layout: :cpu
+
   child_svg = data.child.map {|c| c.to_s.empty? ? nil : 'child.svg' }
-  # svg layout: children
-  svg file: child_svg, layout: :child
+  svg layout: :child, file: child_svg
+  text layout: :child_text, str: data.child
 
+  svg layout: :action_scan,
+      file: data.scan.map { |s| s == 'Y' ? 'icons.svg' : nil }
 
-  svg layout: :action_scan
-  svg layout: :action_broadcast
-  svg layout: :action_crack
-  svg layout: :action_script
+  svg layout: :action_broadcast,
+      file: data.broadcast.map { |s| s == 'Y' ? 'icons.svg' : nil }
 
+  svg layout: :action_crack,
+      file: data.crack.map { |s| s == 'Y' ? 'icons.svg' : nil }
 
-
+  svg layout: :action_script,
+      file: data.script.map { |s| s == 'Y' ? 'icons.svg' : nil }
 
   safe_zone
   cut_zone
