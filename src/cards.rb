c0218f12744208ca00fd9207e94d07dbebd7662a
Andy Meneely <andy@se.rit.edu>
2025-03-12 09:51:40 -0400

    getting there!

--- src/cards.rb
+++ src/cards.rb
@@ -3,9 +3,9 @@ require_relative 'version'
 
 data = Squib.csv file: 'data/cards.csv' do |h,v|
   if h == 'name'
-    v.downcase.gsub(' ','_')
+    v.to_s.downcase.gsub(' ','_')
   elsif h == 'type'
-    v.downcase
+    v.to_s.downcase
   else
     v
   end
@@ -19,40 +19,29 @@ def range_any(arr)
   return result
 end
 
+def dot_svg(arr)
+  arr.map do |f|
+    f.to_s.empty? ? nil : "#{f}.svg"
+  end
+end
+
 Squib::Deck.new(cards: data.nrows) do
   use_layout file: 'layouts/cards.yml'
   background color: :white
-  svg file: 'card_backdrop.svg'
+  svg file: 'card_master.svg'
 
   text str: data.name, layout: :name
   text str: data.type, layout: :type
   text str: data.cpu, layout: :cpu
 
-  child_svg = data.child.map {|c| c.to_s.empty? ? nil : 'child.svg' }
-  svg layout: :child, file: child_svg
-  text layout: :child_text, str: data.child
+  svg layout: :quick, file: dot_svg(data.quick_icon)
+  svg layout: :bots,  file: dot_svg(data.bots_icon)
+  svg layout: :dev,   file: dot_svg(data.dev_icon)
 
-  # rect layout: :scan_box, range: range_any(data.scan)
-  # rect layout: :crack_box, range: range_any(data.crack)
-  # rect layout: :script_box, range: range_any(data.script)
-
-  # svg layout: :vuln_scan, file: 'icons.svg', range: range_any(data.scan)
-  # svg layout: :vuln_crack, file: 'icons.svg', range: range_any(data.crack)
-  # svg layout: :vuln_script, file: 'icons.svg', range: range_any(data.script)
-
-  # text str: data.scan, layout: :scan_text
-  # text str: data.crack, layout: :crack_text
-  # text str: data.script, layout: :script_text
-
-  # svg layout: :action_broadcast,
-  #     file: data.broadcast.map { |s| s == 'Y' ? 'icons.svg' : nil }
-
-  # svg layout: :action_crack,
-  #     file: data.crack.map { |s| s == 'Y' ? 'icons.svg' : nil }
-
-  # svg layout: :action_script,
-  #     file: data.script.map { |s| s == 'Y' ? 'icons.svg' : nil }
-
+  text layout: :quick_text, str: data.quick_text
+  text layout: :bots_text,  str: data.bots_text
+  text layout: :ops_text,   str: data.ops
+  text layout: :dev_text,   str: data.dev_text
 
   # safe_zone
   cut_zone
